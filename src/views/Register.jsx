1451eb5debc6c3c6ff2b4c7f9a60256b5ddde6a5
Dusit Pakdeekunakorn <S6552410011@sau.ac.th>
2025-02-19 16:31:37 +0700

    beta1

---
+++ src/views/Register.jsx
@@ -0,0 +1,130 @@
+import {React, useState} from "react";
+import {
+  Box,
+  Typography,
+  Avatar,
+  TextField,
+  Button,
+  styled
+} from "@mui/material";
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { Link } from "react-router-dom";
+import Travel from "./../assets/travel.png";
+import Profile from "./../assets/profile.png";
+
+function Register() {
+  const [travellerImage, setTravellerImage] = useState(null);
+  const [travellerFullname, setTravellerFullname] = useState('');
+  const [travellerEmail, setTravellerEmail] = useState('');
+  const [travellerPassword, setTravellerPassword] = useState('');
+
+
+  const handleSelectFileClick = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+        setTravellerImage(file);
+    }
+
+
+  }
+
+  const handleRegisterClick = async (e) => {
+    console.log(travellerFullname, travellerEmail, travellerPassword);
+    //Validate Register Button
+    e.preventDefault();
+    if(travellerFullname.trim().length == 0){
+          alert('ป้อนชื่อ-นามสกุลด้วย') 
+      }else if(travellerEmail.trim().length == 0){ 
+          alert('ป้อนอีเมล์ด้วย') 
+      }else if(travellerPassword.trim().length == 0){ 
+          alert('ป้อนรหัสผ่านด้วย') 
+      }else if(travellerImage == null) { 
+          alert('เลือกรูปด้วย') 
+      }else{ 
+      //ส่งข้อมูลไปให้ API บันทึงลง DB แล้ว redirect ไปหน้า Login
+      }
+  }
+
+  //select image func
+  const SelectFileBt = styled('input')({
+    clip: "rect(0 0 0 0)",
+    clipPath: "inset(50%)",
+    height: 1,
+    overflow: "hidden",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    whiteSpace: "nowrap",
+    width: 1,
+  })
+
+  return (
+    <Box sx={{width: '100%', height:'100vh' ,display:'flex', alignItems: 'center'}}>
+      <Box sx={{width:'60%', boxShadow: 4, mx:'auto', my:'auto' ,p:5}}>
+      {/* Head text =====================================*/}
+      <Typography variant='h5' sx={{textAlign: 'center', fontWeight: 'bold'}}>
+          Travel DTI
+        </Typography>
+
+        {/* Logo image =====================================*/}
+        <Avatar src={Travel} alt='travel logo'
+        sx={{width: 150, height: 150, mx: 'auto', my: 5}}></Avatar>
+
+        {/* Login text =====================================*/}
+        <Typography variant='h5' sx={{textAlign: 'center', fontWeight: 'bold' }}>
+          เข้าใช้งานระบบ
+        </Typography>
+
+        
+        <Typography sx={{fontWeight: 'bold',mt:4, mb:1}}>
+          ชื่อ-สกุล
+        </Typography>
+        {/* TextField Fullname  =====================================*/}
+        <TextField fullWidth value={travellerFullname} onChange={(e) => setTravellerFullname(e.target.value)}/>
+
+        <Typography sx={{fontWeight: 'bold',mt:2, mb:1}}>
+          ชื่อผู้ใช้ (Email)
+        </Typography>
+        {/* TextField Username  =====================================*/}
+        <TextField fullWidth value={travellerEmail} onChange={(e) => setTravellerEmail(e.target.value)}/>
+
+        <Typography sx={{fontWeight: 'bold',mt:2 , mb:1}}>
+          รหัสผ่าน
+        </Typography>
+         {/* TextField passwords  =====================================*/}
+        <TextField fullWidth type='password' value={travellerPassword} onChange={(e) => setTravellerPassword(e.target.value)}/>
+        
+        {/* Profile Image =====================================*/}
+        <Avatar src={travellerImage == null ? Profile : URL.createObjectURL(travellerImage)}alt='travel logo' sx={{width: 150, height: 150, mx: 'auto', my: 5}}></Avatar>
+
+        {/* btSelectFile   =====================================*/}
+        <Box sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}>
+          <Button variant="contained" startIcon={<CloudUploadIcon />} sx={{mx:'auto'}} component="label">
+          อัปโหลดรูปภาพโปรไฟล์
+          <SelectFileBt type="file" accept="image/*" onChange={handleSelectFileClick}/>
+          </Button>
+          </Box>
+        
+
+       
+
+        {/* btRegister   =====================================*/}
+        <Button variant='contained' fullWidth sx={{mt:2, py:2, backgroundColor: '#259e69'}} onClick={handleRegisterClick} >
+            Register
+        </Button>
+
+        <Link to='/'style={{textDecoration: 'none', color: '#259e69'}}>
+        <Typography sx={{fontWeight: 'bold',mt:2 , mb:1, textAlign: 'center'}}>
+          กลับไปหน้าLogin
+        </Typography>
+        </Link>
+
+      </Box>
+    </Box>
+  )
+}
+
+export default Register
